7dc79691538a6922428776fe406e0d13e136ce3a
Lincoln Chawora <lincoln_1996@hotmail.com>
2024-03-22 19:23:00 +0100

    Inital changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// import './index.css';
 import App from './App';
+import StarRating from './components/StarRating';
 import reportWebVitals from './reportWebVitals';
+
+function Test() {
+    const [movieRating, setMovieRating] = useState(0)
+    return (
+        <div>
+            <StarRating maxRating={5} onSetRating={setMovieRating} />
+            <p>The rating is {movieRating} stars</p>
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+    <StarRating maxRating={5} messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]} defaultRating={3} />
+      <Test />
   </React.StrictMode>
 );
 
