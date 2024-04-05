215ddb4284a5c1ac87e3647201dac91c6e07c768
Lincoln Chawora <lincoln_1996@hotmail.com>
2024-04-05 18:58:00 +0100

    Fix function rename

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ export default function App() {
             return;
         }
 
-        handleCloseMovie();
+        fetchMovies();
 
         return function () {
             controller.abort();
